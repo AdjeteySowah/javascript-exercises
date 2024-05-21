fd21c1f1e543d4a943d509cd4b3b0abe60aa6b06
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2024-05-21 14:22:21 +0000

    Write a logic to add individual numbers of a range in order

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(par1, par2) {
+    let sum = 0;
 
+    for (let i = par1; i <= par2; i++) {
+            sum += i;
+    }
+
+    for (let j = par1; j >= par2; j--) {
+        sum += j;
+    }
+
+    if (par1 < 0 || typeof(par1) !== "number" || par2 < 0 || typeof(par2) !== "number") {
+        return "ERROR";
+    } else {
+        return sum;
+    }
 };
 
 // Do not edit below this line
