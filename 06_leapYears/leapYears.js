37838629d89bc2db8508b4b06aa655798d759368
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2024-05-22 20:46:33 +0000

    Write a logic to check for leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let startSliceAt = (year).toString().length-2;
+    let endSliceAt = (year).toString().length;
 
+    if (year % 4 === 0 && !((year).toString().slice(startSliceAt, endSliceAt) === "00") ||
+    ((year).toString().slice(startSliceAt, endSliceAt) === "00") && year % 400 === 0 ){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
