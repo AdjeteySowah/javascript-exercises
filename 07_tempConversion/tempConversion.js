cb50b638f90bfc12b14167e92cfc38b4040a786f
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2024-05-22 21:26:03 +0000

    Write a logic for converting degrees from celsius to fahrenheit and vice-versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  let convertedValue = (degreesFahrenheit - 32) * 5/9;
+
+  if (convertedValue % 1 !== 0) {
+    return parseFloat(convertedValue.toFixed(1));
+  } else {
+    return convertedValue;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  let convertedValue = (degreesCelsius * 9/5) + 32;
+  
+  if (convertedValue % 1 !== 0) {
+    return parseFloat(convertedValue.toFixed(1));
+  } else {
+    return convertedValue;
+  }
 };
 
 // Do not edit below this line
@@ -9,3 +23,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+ 
