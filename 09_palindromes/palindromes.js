55c9558b78903b35c9462af14675871dd80aaeee
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2024-07-09 16:10:12 +0000

    Create a function to check if a string is a palindrome or not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const data = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const dataMadeArray = data.split("");
 
+    let refinedString = str
+        .toLowerCase()
+        .split("")
+        .filter((item) => {
+            return dataMadeArray.includes(item);
+        })
+        .join("");
+
+    return refinedString === refinedString.split("").reverse().join("");
 };
 
 // Do not edit below this line
