c164a6c8314a2f0c3f067d9548233b22c08dfc04
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2024-07-18 17:37:45 +0000

    Get a specified fibonacci term

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(items) {
+    let fibonacciArray = [1, 1];
 
+    if (items < 0) {
+        return "OOPS";
+    } else if (items == 0) {
+        return 0;
+    } else if (items == 1) {
+        return 1;
+    } else {
+        for (let i = 0; i < (items - 2); i++) {
+            let nextItem = fibonacciArray[i] + fibonacciArray[i + 1];
+            fibonacciArray.push(nextItem);
+        }
+
+        return fibonacciArray[items - 1];
+    }
 };
 
 // Do not edit below this line
