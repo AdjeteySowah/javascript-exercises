5f1f28e802eb2dae83050582d69b6a8506a283d8
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2024-07-18 21:48:01 +0000

    Get the name of the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let peopleData = people.map((person) => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
 
+        return person;
+    });
+
+    let oldestPerson = peopleData.reduce((oldest, person) => {
+        let age = person.yearOfDeath - person.yearOfBirth;
+        if (age > (oldest.age || 0)) {
+            return {... person, age};
+        }
+
+        return oldest;
+    }, {});
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
